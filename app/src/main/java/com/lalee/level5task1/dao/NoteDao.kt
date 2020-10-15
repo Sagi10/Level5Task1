12af09ef431b6d308f7fb5f431468ca9fd5b8255
Sagi Lalee <Sagi_lalee@hotmail.com>
2020-10-15 15:59:10 +0200

    added NoteDao interface

---
+++ app/src/main/java/com/lalee/level5task1/dao/NoteDao.kt
@@ -0,0 +1,18 @@
+package com.lalee.level5task1.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.lalee.level5task1.model.Note
+
+@Dao
+interface NoteDao {
+
+    @Insert
+    suspend fun insertNote(note: Note)
+
+    @Query("SELECT * FROM Note LIMIT 1")
+    suspend fun getNotePad(): LiveData<Note?>
+
+    @Update
+    suspend fun updateNote(note: Note)
+}
